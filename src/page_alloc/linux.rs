0e2f144acb70a9bed06d4fc012dd510890693c78
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2024-12-22 21:58:11 +0600

    madvise

--- src/page_alloc/linux.rs
+++ src/page_alloc/linux.rs
@@ -9,15 +9,50 @@ pub struct DynamicPageAlloc;
 // Safety: moving the struct doesn't invalidate currently allocated pages
 unsafe impl PageAlloc for DynamicPageAlloc {
     fn alloc_page(&self, size: usize) -> Result<NonNull<[u8]>, AllocError> {
+        assert!(size > 0);
+
         let alloc_size = size.next_multiple_of(1 << 21); // round up to next multiple of 2MB
 
-        match mmap_wrapper(alloc_size) {
-            Ok(page) => Ok(page),
+        let page = match mmap_wrapper(alloc_size) {
+            Ok(page) => page,
             Err(e) => {
                 eprintln!("failed to allocate memory with mmap: {}.\naborting.", e);
                 std::process::abort();
             }
+        };
+
+        // Safety: we know page is valid allocated memory that has more than 0 size.
+        // there is nothing indicating this can cause UB in man page of madvise
+        unsafe {
+            match libc::madvise(
+                page.as_ptr().as_mut_ptr() as *mut libc::c_void,
+                page.len(),
+                libc::MADV_HUGEPAGE,
+            ) {
+                0 => (),
+                -1 => {
+                    let errno = *libc::__errno_location();
+                    let err = std::io::Error::from_raw_os_error(errno);
+
+                    // TODO: maybe shouldn't abort here :)
+                    eprintln!(
+                        "failed to call madvise on allocated memory: {}.\naborting",
+                        err
+                    );
+                    std::process::abort();
+                }
+                x => {
+                    // TODO: maybe shouldn't abort here :)
+                    eprintln!(
+                        "unexpected return value from madvise: {}. Expected 0 or -1\naborting",
+                        x
+                    );
+                    std::process::abort();
+                }
+            }
         }
+
+        Ok(page)
     }
 
     unsafe fn dealloc_page(&self, page: NonNull<[u8]>) {
@@ -33,6 +68,7 @@ unsafe impl PageAlloc for DynamicPageAlloc {
 
 fn mmap_wrapper(size: usize) -> io::Result<NonNull<[u8]>> {
     assert!(size > 0);
+    // Safety: Call format fits mmap manpage, should be safe
     unsafe {
         match libc::mmap(
             std::ptr::null_mut(),
